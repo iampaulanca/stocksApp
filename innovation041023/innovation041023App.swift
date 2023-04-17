d7ab72a1b68cfd0ffe1667fed3c1dde32abb6dc4
g054573 <paul.x.ancajima@kp.org>
2023-04-17 08:39:53 -0700

    first commit

---
+++ innovation041023/innovation041023App.swift
@@ -0,0 +1,17 @@
+//
+//  innovation041023App.swift
+//  innovation041023
+//
+//  Created by Paul Ancajima on 4/17/23.
+//
+
+import SwiftUI
+
+@main
+struct innovation041023App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
