82f0dcfd7f646e77c4c394a75d8b3718835a77e3
Paul Ancajima <paulancajima@yahoo.com>
2023-04-20 22:31:37 -0700

    updated python script

--- .github/workflows/xcresultParser.py
+++ .github/workflows/xcresultParser.py
@@ -7,18 +7,20 @@ import datetime
 
 
 class CodeCoverage:
-    def __init__(self, appName, id, filenames, totalCoverage):
+    def __init__(self, appName, id, filenames, linesCovered, totalLines):
         self.appName = appName
         self.id = id
         self.filenames = filenames
-        self.totalCoverage = totalCoverage
+        self.linesCovered = linesCovered
+        self.totalLines = totalLines
         self.timestamp = datetime.datetime.now()
 
 
 class Filename:
-    def __init__(self, name, coverage):
+    def __init__(self, name, linesCovered, totalLines):
         self.name = name
-        self.coverage = coverage
+        self.linesCovered = linesCovered
+        self.totalLines = totalLines
 
 
 # Create an ArgumentParser object
@@ -61,23 +63,44 @@ else:
         filenames = []
         # Create a CodeCoverage object with the extracted data
 
-        code_coverage = CodeCoverage(appName=xcresult_path, id=1, filenames=filenames,
-                                     totalCoverage=0)
+        code_coverage = CodeCoverage(appName=xcresult_path, id=1, filenames=filenames, linesCovered=0, totalLines=0)
         for match in matches:
             filename = os.path.basename(match[0])
-            coverage = match[2]
+            coverageString = match[2]
+
+            cov_match = re.match(r'(\d+\.\d+)% \((\d+)/(\d+)\)', coverageString)
+            total_coverage = 0
+            lines_covered = 0
+            total_lines = 0
+
+            if cov_match:
+                total_coverage = float(cov_match.group(1))
+                lines_covered = int(cov_match.group(2))
+                total_lines = int(cov_match.group(3))
+
             if filename.endswith(".app"):
                 code_coverage.appName = filename
-                code_coverage.totalCoverage = coverage
+                code_coverage.linesCovered = lines_covered
+                code_coverage.totalLines = total_lines
             else:
-                filename_obj = Filename(name=filename, coverage=coverage)
+                filename_obj = Filename(name=filename, linesCovered=0, totalLines=0)
+                filename_obj.totalLines = total_lines
+                filename_obj.linesCovered = lines_covered
                 filenames.append(filename_obj)
 
         # Access the properties of the CodeCoverage object
         print("AppName: ", code_coverage.appName)
         print("ID: ", code_coverage.id)
         print("Timestamp: ", code_coverage.timestamp)
-        print("Total Coverage: ", code_coverage.totalCoverage)
+        total_coverage = (code_coverage.linesCovered / code_coverage.totalLines) * 100
+        total_coverage_rounded = round(total_coverage, 2)
+        print(f"Lines Covered: {code_coverage.linesCovered}/{code_coverage.totalLines}")
+        print(f"Total Coverage: {total_coverage_rounded}%")
+        print()
         for filename_obj in code_coverage.filenames:
             print("Filename: ", filename_obj.name)
-            print("Coverage: ", filename_obj.coverage)
+            print(f"Lines Covered: {filename_obj.linesCovered}/{filename_obj.totalLines}")
+            total_coverage = (filename_obj.linesCovered / filename_obj.totalLines) * 100
+            total_coverage_rounded = round(total_coverage, 2)
+            print(f"Total Coverage: {total_coverage_rounded}%")
+            print()
